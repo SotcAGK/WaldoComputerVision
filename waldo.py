3d9c0d7a95e100f6217e670b1aecbc1707c4cd89
William Harvey <harveyw25@mycu.concord.edu>
2019-01-30 11:29:14 -0500

    Find waldo using cut out
    renamed main.py to waldo.py
    removed args

--- waldo.py
+++ waldo.py
@@ -5,17 +5,15 @@ import cv2
 
 #https://machinelearningmastery.com/using-opencv-python-and-template-matching-to-play-wheres-waldo/
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--puzzle", required = True,
-	help = "Path to the puzzle image")
-ap.add_argument("-w", "--waldo", required = True,
-	help = "Path to the waldo image")
-args = vars(ap.parse_args())
+print("img location")
+puzzle = "ExampleImages\Waldo2.jpg"
+
+print("sol location")
+waldo = "ExampleImages\Waldo2solution.jpg"
 
 # load the puzzle and waldo images
-puzzle = cv2.imread(args["puzzle"])
-waldo = cv2.imread(args["waldo"])
+puzzle = cv2.imread(puzzle)
+waldo = cv2.imread(waldo)
 (waldoHeight, waldoWidth) = waldo.shape[:2]
 
 # find the waldo in the puzzle
@@ -30,7 +28,7 @@ roi = puzzle[topLeft[1]:botRight[1], topLeft[0]:botRight[0]]
  
 # construct a darkened transparent 'layer' to darken everything
 # in the puzzle except for waldo
-mask = np.zeros(puzzle.shape, dtype = "uint8")
+mask = numpy.zeros(puzzle.shape, dtype = "uint8")
 puzzle = cv2.addWeighted(puzzle, 0.25, mask, 0.75, 0)
 
 # put the original waldo back in the image so that he is
@@ -39,5 +37,4 @@ puzzle[topLeft[1]:botRight[1], topLeft[0]:botRight[0]] = roi
  
 # display the images
 cv2.imshow("Puzzle", imutils.resize(puzzle, height = 650))
-cv2.imshow("Waldo", waldo)
 cv2.waitKey(0)
